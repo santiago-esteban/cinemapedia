809196475a52b2e9e9cf49d5b33f8a1053af0edc
Santiago Esteban <santi31074@gmail.com>
2024-08-09 20:03:47 +0200

    Riverpod Provider

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,11 @@ import 'package:cinemapedia/config/router/app_router.dart';
 import 'package:cinemapedia/config/theme/app_theme.dart';
 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: ".env");
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
