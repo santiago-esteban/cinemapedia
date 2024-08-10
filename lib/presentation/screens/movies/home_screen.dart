5d38f70b7e8c486b1155e3b8531be82143a93047
Santiago Esteban <santi31074@gmail.com>
2024-08-10 22:56:06 +0200

    Custom Navegation Bar

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -12,6 +12,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: _HomeView(),
+      bottomNavigationBar: CustomNavigationBar(),
     );
   }
 }
