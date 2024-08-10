15afbfd06c871253d5f8401fcd67489c649d2509
Santiago Esteban <santi31074@gmail.com>
2024-08-10 17:13:44 +0200

    MoviesWidget, CardSwiper y Slideshow

---
+++ lib/presentation/widgets/movies/movies_slideshow.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:card_swiper/card_swiper.dart';
+
+class MoviesSlideshow extends StatelessWidget {
+  final List<Movie> movies;
+  const MoviesSlideshow({
+    super.key,
+    required this.movies,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 210,
+      width: double.infinity,
+      child: Swiper(
+        viewportFraction: 0.8,
+        scale: 0.9,
+        autoplay: true,
+        itemCount: movies.length,
+        itemBuilder: (context, index) => _Slide(movie: movies[index]),
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final Movie movie;
+  const _Slide({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
