331d66984c2fd3780d3811003312c6acef9e8649
Santiago Esteban <santi31074@gmail.com>
2024-08-15 23:35:16 +0200

    Isar Datasource Impl

--- lib/infrastructure/datasources/isar_datasource_impl.dart
+++ lib/infrastructure/datasources/isar_datasource_impl.dart
@@ -1,22 +1,48 @@
+import 'dart:math';
+
 import 'package:cinemapedia/domain/datasources/isar_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
 
 class IsarDatasourceImpl extends IsarDatasource {
-  @override
-  Future<bool> isMovieFavorite(int movieId) {
-    // TODO: implement isMovieFavorite
-    throw UnimplementedError();
+  late Future<Isar> db;
+
+  IsarDatasourceImpl() {
+    db = openDB();
+  }
+
+  Future<Isar> openDB() async {
+    final dir = await getApplicationCacheDirectory();
+    if (Isar.instanceNames.isEmpty) {
+      return await Isar.open([MovieSchema], inspector: true, directory: dir.path);
+    }
+    return Future.value(Isar.getInstance());
   }
 
   @override
-  Future<List<Movie>> loadMovies({int limit = 10, offset = 0}) {
-    // TODO: implement loadMovies
-    throw UnimplementedError();
+  Future<bool> isMovieFavorite(int movieId) async {
+    final isar = await db;
+    final Movie? isFavoriteMovie = await isar.movies.filter().idEqualTo(movieId).findFirst();
+    return isFavoriteMovie != null;
   }
 
   @override
-  Future<void> toggleFavorite(Movie movie) {
-    // TODO: implement toggleFavorite
-    throw UnimplementedError();
+  Future<List<Movie>> loadMovies({int limit = 10, offset = 0}) async {
+    final isar = await db;
+    return isar.movies.where().offset(offset).limit(limit).findAll();
+  }
+
+  @override
+  Future<void> toggleFavorite(Movie movie) async {
+    final isar = await db;
+    final favoriteMovie = await isar.movies.filter().idEqualTo(movie.id).findFirst();
+    if (favoriteMovie != null) {
+      //* Borrar
+      isar.writeTxnSync(() => isar.movies.deleteSync(favoriteMovie.isarId!));
+      return;
+    }
+    //* Insertar
+    isar.writeTxnSync(() => isar.movies.putSync(movie));
   }
 }
