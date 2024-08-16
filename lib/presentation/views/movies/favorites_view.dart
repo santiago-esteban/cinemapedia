4ffe1c3e721ca4b6da2c0dbe5bc9678f7a8ea06e
Santiago Esteban <santi31074@gmail.com>
2024-08-16 21:41:51 +0200

    Favorites First View

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,6 +1,7 @@
 import 'package:cinemapedia/presentation/presentation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
@@ -32,6 +33,22 @@ class FavoritesViewState extends ConsumerState<FavoritesView> {
   @override
   Widget build(BuildContext context) {
     final favoritesMovies = ref.watch(isarFavoriteMoviesProvider).values.toList();
+    if (favoritesMovies.isEmpty) {
+      final colors = Theme.of(context).colorScheme;
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(Icons.favorite_outline_sharp, size: 60, color: colors.primary),
+            Text('¡Ohh no!', style: TextStyle(fontSize: 30, color: colors.primary)),
+            const Text('¡No tienes películas favoritas!', style: TextStyle(fontSize: 20, color: Colors.black45)),
+            const SizedBox(height: 20),
+            FilledButton.tonal(onPressed: () => context.go('/home/0/'), child: const Text('Buscar mis favoritas')),
+          ],
+        ),
+      );
+    }
     return Scaffold(
       body: MovieMasonry(
         loadNextPage: loadNextPage,
