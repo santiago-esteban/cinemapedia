2d56765fd243ff23a03d03826538ca99715bc799
Santiago Esteban <santi31074@gmail.com>
2024-08-18 11:00:33 +0200

    PopularView

--- lib/presentation/views/movies/popular_view.dart
+++ lib/presentation/views/movies/popular_view.dart
@@ -1,15 +1,30 @@
+import 'package:cinemapedia/presentation/presentation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class PopularView extends StatelessWidget {
+class PopularView extends ConsumerStatefulWidget {
   const PopularView({super.key});
 
   @override
+  PopularViewState createState() => PopularViewState();
+}
+
+class PopularViewState extends ConsumerState<PopularView> with AutomaticKeepAliveClientMixin {
+  @override
   Widget build(BuildContext context) {
+    super.build(context);
+
+    final popularMovies = ref.watch(popularMoviesProvider);
+
+    if (popularMovies.isEmpty) {
+      return const Center(child: CircularProgressIndicator(strokeWidth: 2));
+    }
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Categories View'),
-      ),
-      body: const Center(child: Text('Categorías')),
+      body: MovieMasonry(loadNextPage: () => ref.read(popularMoviesProvider.notifier).loadNextPage(), movies: popularMovies),
     );
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
