4550a2877f4072bffc6ac9a62c66e538a8182f3a
Santiago Esteban <santi31074@gmail.com>
2024-08-21 22:02:10 +0200

    Providers: Loaders Comentarios

--- lib/presentation/providers/loaders/screen_loader_provider.dart
+++ lib/presentation/providers/loaders/screen_loader_provider.dart
@@ -1,14 +1,15 @@
+//* Importaciones de paquetes necesarios.
 import 'package:cinemapedia/presentation/presentation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-//* Proveedor que indica si es la primera carga de datos de películas.
+//* Proveedor que determina si es la primera vez que se cargan los datos de películas
 final screenLoaderProvider = Provider<bool>((ref) {
-  final step1 = ref.watch(nowPlayingMoviesProvider).isEmpty; // Verifica si la lista de películas de "ahora en cines" está vacía.
-  final step2 = ref.watch(upcomingMoviesProvider).isEmpty; // Verifica si la lista de películas de "próximamente" está vacía.
-  final step3 = ref.watch(popularMoviesProvider).isEmpty; // Verifica si la lista de películas populares está vacía.
-  final step4 = ref.watch(topRatedMoviesProvider).isEmpty; // Verifica si la lista de películas más valoradas está vacía.
+  final step1 = ref.watch(nowPlayingMoviesProvider).isEmpty;
+  final step2 = ref.watch(upcomingMoviesProvider).isEmpty;
+  final step3 = ref.watch(popularMoviesProvider).isEmpty;
+  final step4 = ref.watch(topRatedMoviesProvider).isEmpty;
 
-  if (step1 || step2 || step3 || step4) return true; // Si alguna de las listas está vacía, no carga la pantalla de las películas.
+  if (step1 || step2 || step3 || step4) return true; //* Retorna `true` si alguna lista está vacía, indicando que la carga inicial no está completa
 
-  return false; // Si todas las listas tienen datos, carga la pantalla de las películas.
+  return false; //* Retorna `false` si todas las listas tienen datos, indicando que la carga inicial está completa
 });
