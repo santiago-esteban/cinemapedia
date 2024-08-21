d445bbd5edbaf8972548e1a89320787dc85c2eb1
Santiago Esteban <santi31074@gmail.com>
2024-08-21 21:29:56 +0200

    Providers: Isar Comentarios

--- lib/presentation/providers/isar/isar_favorite_movies_provider.dart
+++ lib/presentation/providers/isar/isar_favorite_movies_provider.dart
@@ -1,18 +1,22 @@
+//* Importaciones de paquetes necesarios.
 import 'package:cinemapedia/domain/domain.dart';
 import 'package:cinemapedia/presentation/providers/isar/isar_repository_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+//* Proveedor que gestiona el estado de las películas favoritas utilizando Isar como repositorio
 final isarFavoriteMoviesProvider = StateNotifierProvider<FavoriteMoviesNotifier, Map<int, Movie>>((ref) {
   final isarRepository = ref.watch(isarRepositoryProvider);
   return FavoriteMoviesNotifier(isarRepository: isarRepository);
 });
 
+//* Notifier que maneja la lógica de las películas favoritas
 class FavoriteMoviesNotifier extends StateNotifier<Map<int, Movie>> {
   int page = 0;
   final IsarRepository isarRepository;
 
-  FavoriteMoviesNotifier({required this.isarRepository}) : super({});
+  FavoriteMoviesNotifier({required this.isarRepository}) : super({}); //* Inicializa el estado con un mapa vacío
 
+  //* Carga la siguiente página de películas favoritas desde el repositorio
   Future<List<Movie>> loadNextPage() async {
     final movies = await isarRepository.loadMovies(offset: page * 10, limit: 20);
     page++;
@@ -20,18 +24,19 @@ class FavoriteMoviesNotifier extends StateNotifier<Map<int, Movie>> {
     for (final movie in movies) {
       tempMoviesMap[movie.id] = movie;
     }
-    state = {...state, ...tempMoviesMap};
+    state = {...state, ...tempMoviesMap}; //* Actualiza el estado con las nuevas películas
     return movies;
   }
 
+  //* Alterna el estado de una película entre favorita y no favorita
   Future<void> toggleFavorite(Movie movie) async {
-    await isarRepository.toggleFavorite(movie);
+    await isarRepository.toggleFavorite(movie); //* Actualiza el estado en el repositorio
     final bool isMovieInFavorites = state[movie.id] != null;
     if (isMovieInFavorites) {
-      state.remove(movie.id);
+      state.remove(movie.id); //* Elimina la película de favoritos si ya está en el estado
       state = {...state};
     } else {
-      state = {...state, movie.id: movie};
+      state = {...state, movie.id: movie}; //* Añade la película a favoritos si no está en el estado
     }
   }
 }
--- lib/presentation/providers/isar/isar_repository_provider.dart
+++ lib/presentation/providers/isar/isar_repository_provider.dart
@@ -1,6 +1,8 @@
+//* Importaciones de paquetes necesarios.
 import 'package:cinemapedia/infrastructure/infrastructure.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+//* Proveedor que crea una instancia de IsarRepositoryImpl, utilizando IsarDatasourceImpl como fuente de datos
 final isarRepositoryProvider = Provider((ref) {
   return IsarRepositoryImpl(datasource: IsarDatasourceImpl());
 });
