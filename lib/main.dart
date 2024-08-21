8948b0652a36311b7b46c45b72c365566b748fad
Santiago Esteban <santi31074@gmail.com>
2024-08-21 21:22:38 +0200

    Main Comentarios

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,28 @@
+//* Importaciones de paquetes necesarios.
 import 'dart:async';
 
-import 'package:cinemapedia/config/router/app_router.dart'; // Importa la configuración del enrutador de la aplicación.
-import 'package:cinemapedia/config/theme/app_theme.dart'; // Importa la configuración del tema de la aplicación.
+import 'package:cinemapedia/config/router/app_router.dart';
+import 'package:cinemapedia/config/theme/app_theme.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart'; // Importa el paquete para manejar variables de entorno.
-import 'package:flutter_riverpod/flutter_riverpod.dart'; // Importa Riverpod para la gestión del estado.
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+//* Función principal que inicializa la aplicación
 Future<void> main() async {
-  await dotenv.load(fileName: ".env"); // Carga las variables de entorno desde el archivo .env.
-  runApp(const ProviderScope(child: MyApp())); // Ejecuta la aplicación y envuelve el widget principal en `ProviderScope` para que Riverpod pueda manejar el estado.
+  await dotenv.load(fileName: ".env"); //* Carga las variables de entorno
+  runApp(const ProviderScope(child: MyApp())); //* Ejecuta la aplicación dentro de un `ProviderScope` para usar Riverpod
 }
 
+//* Widget principal de la aplicación
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      routerConfig: appRouter, // Configura el enrutador de la aplicación con la configuración definida en `appRouter`.
-      debugShowCheckedModeBanner: false, // Desactiva el banner de depuración que aparece en el modo de depuración.
-      theme: AppTheme().getTheme(), // Establece el tema de la aplicación utilizando la configuración proporcionada por `AppTheme`.
+      debugShowCheckedModeBanner: false,
+      routerConfig: appRouter, //* Configura las rutas de la aplicación
+      theme: AppTheme().getTheme(), //* Aplica el tema personalizado de la aplicación
     );
   }
 }
