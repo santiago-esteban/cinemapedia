831f0135fce010bc7ae91238bcf0301ff5e97f45
Santiago Esteban <santi31074@gmail.com>
2024-08-21 21:26:28 +0200

    Providers: Actors Comentarios

--- lib/presentation/providers/actors/actors_movie_provider.dart
+++ lib/presentation/providers/actors/actors_movie_provider.dart
@@ -1,26 +1,30 @@
+//* Importaciones de paquetes necesarios.
 import 'package:cinemapedia/domain/domain.dart';
 import 'package:cinemapedia/presentation/presentation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+//* Proveedor que gestiona la lista de actores por película
 final actorsMovieProvider = StateNotifierProvider<ActorsMovieNotifier, Map<String, List<Actor>>>((ref) {
   final actorsRepository = ref.watch(actorsRepositoryProvider);
-
   return ActorsMovieNotifier(getActors: actorsRepository.getActorByMovie);
 });
 
+//* Función para obtener actores basado en el ID de la película
 typedef GetActorsCallback = Future<List<Actor>> Function(String movieId);
 
+//* Notifier que maneja el estado de los actores para una película específica
 class ActorsMovieNotifier extends StateNotifier<Map<String, List<Actor>>> {
   final GetActorsCallback getActors;
 
   ActorsMovieNotifier({
     required this.getActors,
-  }) : super({});
+  }) : super({}); //* Inicializa el estado como un mapa vacío
 
+  //* Carga los actores para una película específica si aún no están en el estado
   Future<void> loadActors(String movieId) async {
-    if (state[movieId] != null) return;
+    if (state[movieId] != null) return; //* Evita recargar si ya se han cargado los actores para esa película
 
     final List<Actor> actors = await getActors(movieId);
-    state = {...state, movieId: actors};
+    state = {...state, movieId: actors}; //* Actualiza el estado con la lista de actores
   }
 }
--- lib/presentation/providers/actors/actors_repository_provider.dart
+++ lib/presentation/providers/actors/actors_repository_provider.dart
@@ -1,7 +1,8 @@
+//* Importaciones de paquetes necesarios.
 import 'package:cinemapedia/infrastructure/infrastructure.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-//* Proveedor de instancia para el repositorio de actores.
+//* Crea una instancia de ActorRepositoryImpl utilizando ActorDatasourceImpl como fuente de datos.
 final actorsRepositoryProvider = Provider((ref) {
-  return ActorRepositoryImpl(ActorDatasourceImpl()); // Crea una instancia de ActorRepositoryImpl utilizando ActorDatasourceImpl como fuente de datos.
+  return ActorRepositoryImpl(ActorDatasourceImpl());
 });
