c7a87f1690560356d850313164e327d6439744bd
Santiago Esteban <santi31074@gmail.com>
2024-08-21 22:21:38 +0200

    Fin Comentarios

--- lib/presentation/providers/search/search_movies_provider.dart
+++ lib/presentation/providers/search/search_movies_provider.dart
@@ -1,36 +1,37 @@
+//* Importaciones de paquetes necesarios.
 import 'package:cinemapedia/domain/domain.dart';
 import 'package:cinemapedia/presentation/presentation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-//* Proveedor de estado para manejar la consulta de búsqueda de películas.
-final searchQueryProvider = StateProvider<String>((ref) => ''); // Inicializa el estado de la consulta de búsqueda como una cadena vacía.
+//* Proveedor para manejar la consulta de búsqueda de películas
+final searchQueryProvider = StateProvider<String>((ref) => '');
 
-//* Proveedor de estado para manejar la lista de películas buscadas.
+//* Proveedor para manejar la lista de películas buscadas
 final searchedMoviesProvider = StateNotifierProvider<SearchedMoviesNotifier, List<Movie>>((ref) {
-  final movieRepository = ref.read(movieRepositoryProvider); // Obtiene el repositorio de películas del proveedor.
+  final movieRepository = ref.read(movieRepositoryProvider);
   return SearchedMoviesNotifier(
-    searchMovies: movieRepository.searchMovies, // Pasa la función de búsqueda de películas al notifier.
-    ref: ref, // Pasa la referencia al proveedor para acceder a otros proveedores.
+    searchMovies: movieRepository.searchMovies,
+    ref: ref,
   );
 });
 
-typedef SearchMoviesCallBack = Future<List<Movie>> Function(String query); // Callback para la búsqueda de películas por consulta.
+typedef SearchMoviesCallBack = Future<List<Movie>> Function(String query); //* Callback para la búsqueda de películas
 
-//* Clase que maneja el estado de las películas buscadas y la lógica de búsqueda.
+//* Notifier que maneja el estado de las películas buscadas y la lógica de búsqueda
 class SearchedMoviesNotifier extends StateNotifier<List<Movie>> {
-  final SearchMoviesCallBack searchMovies; // Callback para buscar películas basado en la consulta.
-  final Ref ref; // Referencia al objeto de estado para acceder a otros proveedores.
+  final SearchMoviesCallBack searchMovies;
+  final Ref ref;
 
   SearchedMoviesNotifier({
     required this.searchMovies,
     required this.ref,
   }) : super([]);
 
-  //* Método para buscar películas basado en la consulta proporcionada.
+  //* Método para realizar la búsqueda de películas según la consulta proporcionada
   Future<List<Movie>> searchMoviesByQuery(String query) async {
-    final List<Movie> movies = await searchMovies(query); // Ejecuta la búsqueda de películas.
-    ref.read(searchQueryProvider.notifier).update((state) => query); // Actualiza la consulta de búsqueda en el proveedor.
-    state = movies; // Actualiza el estado con las películas encontradas.
-    return movies; // Retorna la lista de películas encontradas.
+    final List<Movie> movies = await searchMovies(query); //* Ejecuta la búsqueda de películas
+    ref.read(searchQueryProvider.notifier).update((state) => query); //* Actualiza la consulta de búsqueda en el proveedor
+    state = movies; //* Actualiza el estado con las películas encontradas
+    return movies;
   }
 }
